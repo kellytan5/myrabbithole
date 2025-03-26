308f89fd111887c2fa3a230db6b63ffe4f5c148d
Kelly <kellytan05925@gmail.com>
2025-03-25 21:28:11 -0400

    mini menu alignment fix

--- src/components/mini_menu.vue
+++ src/components/mini_menu.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="mini-menu-container">
-    <div class="menu-button" @click="aboutme">
+    <div class="menu-button" style="margin: 10% 5% 5% 5%;" @click="aboutme">
         <p>About Me</p>
     </div>
     <div class="menu-button" @click="education">
